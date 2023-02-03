fb1607a886f419507897a42562745501f167083f
ramxsh <91254304+ramxsh@users.noreply.github.com>
2023-02-03 15:44:56 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,7 +4,7 @@ class Config(object):
     API_ID = int(os.environ.get("APP_ID", "28382321"))
     API_HASH = os.environ.get("API_HASH", "a1340b7972df975470aee26ea4ad6bfb")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5895084879:AAEfB-A862ol9lU0LyIh1HWNW3GfJ9JCOb8")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "BAGxFHEALaWH-2dgzFxhbD6FhQTeciYcUXM81Fpv0dsTRqLuTgZTryodLGxhdsKM1X8KoHSGxN_NozXpCmxlNK7xvzIqEKWLdR1q2n8rWwvZ7H9_1XzUq1Q7jXis5QbtSetK2UECkSbGXg6raRUnJxb3uXs2l55mZnrtWbvo7eUEugZcb4r31OrOjGWLPSGqL3rlDp6QDltqUFnk7LhCUnFdDl68k7AZmENhEl1en874CBHI61dLuxE8bRATWociHncPFRNiJMKRq1Z3Go3IxDzYIdrpxG_OhaiyT_1HWFb0ijHqByb0tBPXHw51rIVYn5JJ6E2el59gsEfkBALBweEkHWS7egAAAAFI65DaAA")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1AZWarzcBuzuuIdh5Age1VKqmJGudoChiNwcbk_SNsDZ3kR7HTJxU8VObbhqgig0MoY4KfskE1psThQNEKlmjcTe7R8boQfbBesa-wjJWg0-_cNiHjUjdvTLS_H92v9NTlsqswQrXI3Es5DLUXfv8ElF47uXWNiEYNIdPX37TmatFapagU6UYd-VM3etulaOtYS9-8PNnWjePK5HUK1CPyB3hpgWUGxt-likbbkO69T_fBP_fMpAVkjbd1Vp4qX3Es9pqAy4uxy9Yw2AI0YISiMpqa6j5cgSNoIKIYYkjKLVxAJ8MFTd4VR0pmQG23Z5PpE7ZhQ0j5E9lxEyBqbIWHifl-0qdx0M=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "vpstrialbot")
